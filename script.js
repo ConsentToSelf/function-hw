1f15d252a38ffbe785252320f7288555c1720f2d
Султан <carameleclipsimus@gmail.com>
2023-02-02 00:11:32 +0600

    additionally hw

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ console.log(result);
 // ! Задание второе
 
 const proparityCheck = (check) => {
-    check = prompt("Введите число");
     if (check % 2 === 0) {
         console.log("Число четное");
     } else {
@@ -20,42 +19,40 @@ const proparityCheck = (check) => {
     }
     return check;
 };
-proparityCheck();
+let answer = proparityCheck(3);
+console.log(answer);
+// // ! Задание 3.1
 
-// ! Задание 3.1
-
-const number = (num = prompt("Введите цифру, я возвращу квадрат")) => {
-    let result = num * num;
-    return result;
-};
-console.log(number());
-//  ! Задание 3.2
 const number = (num) => {
     let result = num * num;
     return result;
 };
-number();
+console.log(number(4));
+
+//  ! Задание 3.2
+// const number = (num) => {
+//     let result = num * num;
+//     return result;
+// };
+// number();
 
 // ! Задание четвертое
 
 const checkYearsOld = (question) => {
-    question = prompt("Сколько вам лет?");
-    if (question === isNaN) {
+    if (isNaN(question)) {
         console.log("Вы ввели неправильное значение!");
     } else if (question <= 12) {
         console.log("Привет, друг!");
     } else {
-        console.log("Добро пожаловать");
+        console.log("Добро пожаловать!");
     }
     return question;
 };
-checkYearsOld();
+checkYearsOld(12);
 
 // ! Задание пятое
 
 const twoNum = (a, b) => {
-    a = prompt("Введите первое число");
-    b = prompt("Введите второе число");
     if (isNaN(a) && isNaN(b)) {
         console.log("Одно или оба значения не являются числом");
     } else {
@@ -63,39 +60,18 @@ const twoNum = (a, b) => {
         console.log(result);
     }
 };
-twoNum();
+twoNum(3, 5);
 
-// ! Задание шестое
+// // ! Задание шестое
 
 const returnSquare = (num) => {
-    num = prompt("Введите число");
     if (isNaN(num)) {
         console.log("Переданный параметр не является числом");
     } else if (num > 10) {
         console.log("необходимо ввести число меньше 10");
     } else {
         let result = Math.pow(num, 3);
-        return console.log(`n в кубе равняется ${result}`);
+        return console.log(`${num} в кубе равняется ${result}`);
     }
 };
-returnSquare();
-
-// ! Задание 7
-
-const seasonsTimes = (num) => {
-    num = Number(prompt("Введите число месяца"));
-    if (num == 1 || num == 2 || num == 12) {
-        console.log("Зима");
-    } else if (num > 2 && num < 6) {
-        console.log("Весна");
-    } else if (num > 6 && num < 9) {
-        console.log("Лето");
-    } else if (num == 9 || num == 10 || num == 11) {
-        console.log("Осень");
-    } else if (isNaN(num)) {
-        console.log("Переданный параметр не является числом");
-    } else {
-        console.log("Такого месяца не существует");
-    }
-};
-seasonsTimes();
+returnSquare(4);
